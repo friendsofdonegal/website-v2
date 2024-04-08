978202398589f9f9ceb5bbce132dc95bea34f19a
Michael Tyson <myty@users.noreply.github.com>
2024-04-08 00:36:23 -0400

    start new layout for homepage

--- packages/ui/src/components/header/header.tsx
+++ packages/ui/src/components/header/header.tsx
@@ -6,52 +6,55 @@ import { MenuButton } from "../menu-button/menu-button";
 import "./header.css";
 
 interface HeaderProps {
-    logo?: ReactNode;
-    donateButton?: ReactNode;
-    mobileDonateButton?: ReactNode;
-    menuItems?: { label: string; path: string }[];
+  logo?: ReactNode;
+  donateButton?: ReactNode;
+  mobileDonateButton?: ReactNode;
+  menuItems?: { label: string; path: string }[];
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
-    const { donateButton, mobileDonateButton, logo, menuItems } = props;
-    const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
+  const { donateButton, mobileDonateButton, logo, menuItems } = props;
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
 
-    return (
-        <div className="header">
-            <div className="header__content">
-                <a href="/">{logo}</a>
-                <ul className="mr-4 flex-grow justify-end gap-x-8 gap-y-4 md:flex md:flex-row">
-                    {menuItems?.map((item, id) => (
-                        <NavMenuItem className="hidden md:inline" key={`id-${id}`} path={item.path}>
-                            {item.label}
-                        </NavMenuItem>
-                    ))}
-                </ul>
-                {donateButton ?? (
-                    <Button className="hidden md:inline" variant="tertiary">
-                        Donate
-                    </Button>
-                )}
-                <MenuButton
-                    className="md:hidden"
-                    isOpen={mobileMenuOpen}
-                    onClick={() => setMobileMenuOpen((open) => !open)}
-                />
-            </div>
-            {mobileMenuOpen && (
-                <div className="header__mobile-menu">
-                    <ul>
-                        {menuItems?.map((item, id) => (
-                            <NavMenuItem key={`id-${id}`} path={item.path}>
-                                {item.label}
-                            </NavMenuItem>
-                        ))}
-                    </ul>
-                    {mobileDonateButton ?? <Button variant="tertiary">Donate</Button>}
-                </div>
-            )}
+  return (
+    <div className="header">
+      <div className="header__content">
+        <a href="/">{logo}</a>
+        <ul className="mx-12 flex-grow justify-start gap-x-12 md:flex md:flex-row">
+          {menuItems?.map((item, id) => (
+            <NavMenuItem
+              className="hidden text-lg uppercase tracking-wider md:inline"
+              key={`id-${id}`}
+              path={item.path}>
+              {item.label}
+            </NavMenuItem>
+          ))}
+        </ul>
+        {donateButton ?? (
+          <Button className="hidden md:inline" variant="tertiary">
+            Donate
+          </Button>
+        )}
+        <MenuButton
+          className="md:hidden"
+          isOpen={mobileMenuOpen}
+          onClick={() => setMobileMenuOpen((open) => !open)}
+        />
+      </div>
+      {mobileMenuOpen && (
+        <div className="header__mobile-menu">
+          <ul>
+            {menuItems?.map((item, id) => (
+              <NavMenuItem key={`id-${id}`} path={item.path}>
+                {item.label}
+              </NavMenuItem>
+            ))}
+          </ul>
+          {mobileDonateButton ?? <Button variant="tertiary">Donate</Button>}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export { Header };
