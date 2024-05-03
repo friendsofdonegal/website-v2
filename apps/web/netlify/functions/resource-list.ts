c8af479a776e057419ec1f31337c9a0468e37a2f
Michael Tyson <myty@users.noreply.github.com>
2024-05-02 21:27:20 -0400

    Refactor email handling in resource-list.ts

--- apps/web/netlify/functions/resource-list.ts
+++ apps/web/netlify/functions/resource-list.ts
@@ -1,6 +1,10 @@
-import type { Handler, HandlerEvent } from "@netlify/functions";
 import mailchimp from "@mailchimp/mailchimp_marketing";
-import { Emailer } from "netlify/functions/emailer";
+import crypto from "crypto";
+import sgMail, { type MailDataRequired } from "@sendgrid/mail";
+import { BlobServiceClient } from "@azure/storage-blob";
+import { render } from "@react-email/components";
+import ResourceListEmail from "@fod/emails/emails/resource-list";
+import "@netlify/functions";
 
 interface HandlerBody {
   email?: string;
@@ -13,26 +17,26 @@ const headers = {
   "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
 };
 
-export const handler: Handler = async function (event: HandlerEvent) {
+export default async (request: Request) => {
   try {
-    if (event.httpMethod === "OPTIONS") {
-      return {
+    if (request.method === "OPTIONS") {
+      return Response.json({
         statusCode: 204,
         headers,
-      };
+      });
     }
 
-    const { email, optIn = false }: HandlerBody = event.body ? JSON.parse(event.body) : {};
+    const { email, optIn = false }: HandlerBody = await request.json();
 
     if (!email) {
-      return {
+      return Response.json({
         statusCode: 400,
         headers,
         body: JSON.stringify({
           status: "error",
           error: "Email is required",
         }),
-      };
+      });
     }
 
     const emailer = new Emailer();
@@ -50,23 +54,23 @@ export const handler: Handler = async function (event: HandlerEvent) {
 
     await emailer.sendResourceList(email);
 
-    return {
+    return Response.json({
       statusCode: 200,
       headers,
       body: JSON.stringify({
         status: "success",
       }),
-    };
+    });
   } catch (e: any) {
     console.error(e);
-    return {
+    return Response.json({
       statusCode: 500,
       headers,
       body: JSON.stringify({
         status: "error",
         error: "An error occurred.",
       }),
-    };
+    });
   }
 };
 
@@ -74,4 +78,104 @@ function isSuccess(
   response: mailchimp.lists.MembersSuccessResponse | mailchimp.ErrorResponse,
 ): response is mailchimp.lists.MembersSuccessResponse {
   return (response as any).email_address;
+}
+
+class Emailer {
+  readonly #azureStorageConnection: string;
+  readonly #mailchimpListId: string;
+
+  constructor() {
+    const {
+      AZURE_STORAGE_CONNECTION,
+      MAILCHIMP_API_KEY,
+      MAILCHIMP_SERVER_PREFIX,
+      MAILCHIMP_LIST_ID,
+      SENDGRID_API_KEY,
+    } = Netlify.env.toObject();
+
+    if (!SENDGRID_API_KEY) {
+      throw new Error("SendGrid API key is missing");
+    }
+
+    if (!AZURE_STORAGE_CONNECTION) {
+      throw new Error("Azure Storage connection string is missing");
+    }
+
+    if (!MAILCHIMP_LIST_ID) {
+      throw new Error("Mailchimp list ID is missing");
+    }
+
+    sgMail.setApiKey(SENDGRID_API_KEY);
+
+    mailchimp.setConfig({
+      apiKey: MAILCHIMP_API_KEY,
+      server: MAILCHIMP_SERVER_PREFIX,
+    });
+
+    this.#azureStorageConnection = AZURE_STORAGE_CONNECTION;
+    this.#mailchimpListId = MAILCHIMP_LIST_ID;
+  }
+
+  subscribeToMailingList(email: string) {
+    const subscriberHash = crypto.createHash("md5").update(email).digest("hex");
+    return mailchimp.lists.setListMember(
+      this.#mailchimpListId,
+      subscriberHash,
+      {
+        email_address: email,
+        status_if_new: "pending",
+      },
+      { skipMergeValidation: true },
+    );
+  }
+
+  async sendResourceList(email: string) {
+    const msg: MailDataRequired = {
+      to: email,
+      from: { name: "Friends of Donegal", email: "info@friendsofdonegal.org" },
+      subject: "Friends of Donegal - Community Resource List",
+      html: render(ResourceListEmail()),
+      attachments: [await this.createPdfAttachment("Community-Resource-List.pdf")],
+    };
+
+    await sgMail.send(msg);
+  }
+
+  private async createPdfAttachment(filename: string) {
+    // Create the BlobServiceClient object with connection string
+    const blobServiceClient = BlobServiceClient.fromConnectionString(this.#azureStorageConnection);
+
+    const containerClient = blobServiceClient.getContainerClient("pdfs");
+    const blobClient = containerClient.getBlockBlobClient("Community-Resource-List.pdf");
+
+    const { readableStreamBody } = await blobClient.download();
+    if (!readableStreamBody) {
+      throw new Error("Failed to download blob");
+    }
+
+    const content = await this.streamToBase64String(readableStreamBody);
+
+    return {
+      type: "application/pdf",
+      disposition: "attachment",
+      content,
+      filename,
+    };
+  }
+
+  private async streamToBase64String(readableStream: NodeJS.ReadableStream) {
+    return new Promise<string>((resolve, reject) => {
+      const chunks: Buffer[] = [];
+
+      readableStream.on("data", (data) => {
+        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+      });
+
+      readableStream.on("end", () => {
+        resolve(Buffer.concat(chunks).toString("base64"));
+      });
+
+      readableStream.on("error", reject);
+    });
+  }
 }
